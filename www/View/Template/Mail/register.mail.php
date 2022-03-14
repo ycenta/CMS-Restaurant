0e27e4fc85eef8558532074c68ccf6e4faaa4b3a
Yohan Centanni <thorbenliya@gmail.com>
2022-03-15 00:47:01 +0100

    add mail template and custom mail method

--- www/View/Template/Mail/register.mail.php
+++ www/View/Template/Mail/register.mail.php
@@ -1,5 +1,7 @@
 <html>
 
-<p> {{ variable }} {{ variable }} </p>
+<h1>Bienvenue <?php echo $variables['name'] ?></h1>
+<p>Veuillez confirmer votre inscription en cliquant sur le lien <span> <?php echo $variables['url'] ?></span> </p>
+
 
 </html>
