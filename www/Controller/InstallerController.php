24b61c5d2494f68a7a7b2e9d8f8a8bfadc721324
ycenta <thorbenliya@gmail.com>
2022-07-19 00:05:34 +0200

    add installer file

---
+++ www/Controller/InstallerController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use App\Core\View;
+use App\Core\Auth;
+use App\Model\User as UserModel;
+
+
+
+class InstallerController {
+
+    public function __construct()
+    {
+       
+    }
+
+}
